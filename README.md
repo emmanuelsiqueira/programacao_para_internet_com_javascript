806869469e6e21a5a541d925b05a328cecf7c875
Emmanuel Siqueira <emmanuelsiqueira30@gmail.com>
2021-06-24 21:18:01 -0300

    Curso: Programação para internet com Javascript

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# programacao_para_internet_com_javascript
-Programação para internet com Javascript.
+Os arquivos deste repositório são práticas desenvolvidas a partir das aulas do curso Programação para internet com Javascript da plataforma DIGITAL INNOVATION ONE.
+
+DIGITAL INNOVATION ONE
+
+Categoria de cursos: Javascript
+
+Curso: Programação para internet com Javascript
+
+Módulo 1 - Introdução ao Javascript
+
+1.1 - Introdução ao Javascript
+1.2 - Array e Dicionário
+1.3 - Condicionais, laços de repetição e Date
+
+Módulo 2 - Desenvolva páginas web com Javascript
+
+2.1 - Desenvolva páginas web com Javascript
+2.2 - Parte 2: Manipulando elementos da página
---
+++ js/main.js
@@ -0,0 +1,90 @@
+
+var Data = new Date();
+console.log(Data.getDay());
+console.log(Data.getMonth());
+console.log(Data.getFullYear());
+
+
+/*
+var count;
+for (count = 0; count <= 5; count++)
+{
+    console.log(count);
+}
+*/
+/*
+while (count <= 5)
+{
+ console.log(count);
+    count ++;
+}
+*/
+/*
+var idade = prompt("Qual sua idade?");
+if (idade >= 18) {
+    alert("Maior de idade");
+}
+else
+{
+    alert("Menor de idade");
+}
+*/
+
+/* Array e Dicionário
+
+//var frutas = ["pera","maçã","uva"];
+
+// push adiciona o elemento no fim do array
+//frutas.push("banana");
+//console.log(frutas);
+//  console.log(frutas.pop());
+
+// length mostra o número de elementos no array
+//console.log(frutas.length);
+
+// reverse coloca os elementos do array invertidos 
+//console.log(frutas.reverse());
+
+// converte array em string
+//console.log(frutas.toString());
+//var strFrutas = frutas.toString();
+
+// Converte os elementos de uma string para maiúsculas 
+//console.log(strFrutas.toUpperCase());
+
+// Exibe o primeiro elemento do array
+//console.log(frutas[0]);
+
+// Exibe o array como tipo string
+//console.log(strFrutas);
+
+// Exibe o primeiro elemento da string
+//console.log(strFrutas[0]);
+
+// Substitiu o delimitador do array
+//console.log(frutas.join(" - "));
+
+// Variável recebe dicionário (JSON)
+var dicFruta = {nome: "Maçã", cor: "Vermelha"}
+console.log(dicFruta);
+console.log("Nome: " + dicFruta.nome); 
+console.log(dicFruta.cor); 
+
+// Variável recebe dicionário com outros índices (JSON)
+var dicFrutas = [{nome: "Maçã", cor: "Vermelha"},{nome: "Uva", cor: "Roxa"}]
+console.log(dicFrutas);
+console.log(dicFrutas[1].nome); 
+console.log(dicFrutas[1].cor); 
+
+/* Introdução ao Javascript
+
+var nome = "Emmanuel";
+var idade = 33;
+var frase = "Japão";
+
+// Modifica conteúdo da variável usando replace
+var frase2 = frase.replace("Japão","Brasil");
+
+console.log("O melhor time do mundo é o " + frase2);
+console.log("Meu nome é " + nome + "e tenho " + idade + " anos");
+*/
